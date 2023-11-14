9948a891f6c91b3fe2db9b93525dc28c8638d390
Komron <kr135369@gmail.com>
2023-11-14 14:23:06 +0500

    objects/primitives

---
+++ Js_fundamentals_part_1/data_types/index.js
@@ -0,0 +1,18 @@
+let age = 23;
+console.log(age);
+console.log(typeof age); //Number
+
+let firstName = "Komron";
+console.log(firstName);
+console.log(typeof firstName); //String
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun); //Boolean
+
+let year;
+console.log(year);
+console.log(typeof year); //Undefined
+
+
+console.log(typeof null); //Null (legacy!)
