469f6cad174a08ca562a0782edcef606a29db11e
Komron <kr135369@gmail.com>
2023-11-14 14:44:17 +0500

    mutate and immutate variables

---
+++ Js_fundamentals_part_1/let_const_and_var/index.js
@@ -0,0 +1,7 @@
+let age = 30;
+    age = 31; //Mutated variables (not recommended for use!)
+
+const birthYear = 2001;
+    birthYear = 2002; // ERROR! Immutate variable!
+
+// As a best practice for a writing clean code, recommended to use const by default!
