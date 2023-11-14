4ca6ddce8c685b161fa472ec71348d100ca0e906
Komron <kr135369@gmail.com>
2023-11-14 17:20:51 +0500

    Math, Assignment, Comparison operators

---
+++ Js_fundamentals_part_1/basic operators/index.js
@@ -0,0 +1,32 @@
+// Math operators
+const ageJonas = 2037 - 1991;
+                                // that's not good 'cause we're repeating this value
+const ageSarah = 2037 - 2018;
+
+console.log(ageJonas, ageSarah); // 46, 19. Output multiply values 
+
+
+
+const now = 2037;
+
+const ageJonas = now - 1991;
+                                // a better use case for variable right here
+const ageSarah = now - 1991;
+
+console.log(ageJonas, ageSarah); // 46, 19 .
+
+
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + lastName);  // JonasSchmedtman, also we can
+console.log(firstName + ' ' + lastName)  // Jonas Schmedtman
+
+
+// Assignment operators
+let x = 10 + 5; // 15
+    x += 10; // x = x + 10 (25)
+    x *= 4; // x = x * 4 (60)
+    x++; // x = x + 1 (16)
+    x--; // x = x - 1 (14) 
+
