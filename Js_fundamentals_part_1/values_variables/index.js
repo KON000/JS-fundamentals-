73eac0fe01723f30094ab44ce21c77c91680d041
Komron <kr135369@gmail.com>
2023-11-14 14:01:38 +0500

    declare variables

---
+++ Js_fundamentals_part_1/values_variables/index.js
@@ -0,0 +1,13 @@
+let js = "amazing";
+console.log(42+10+6+7);
+
+console.log("jonas");
+console.log(23);
+let firstname = "jonas"
+console.log(firstname)
+console.log(firstname)
+console.log(firstname)
+
+let myFirstJob = "Teacher";
+let myCurrentJob = "Coder";3
+
