91e57d588aeefed7b2430194679a080021918304
Komron <kr135369@gmail.com>
2023-11-15 14:47:02 +0500

    higher bmi

---
+++ Js_fundamentals_part_1/code chalenges/index.js
@@ -0,0 +1,41 @@
+// const weightsMarks = '78';
+// const heightsMarks = '1.69';
+// const weightsJohns = '92';
+// const heigthsJohns = '195';
+
+/*class BMIPerson {
+    constructor({weight, height}){
+        this.weight = weight;
+        this.height = height;
+    }
+
+    calculateBMI = function() {
+        return this.weight / (this.height * this.height);
+    }
+}
+
+const Mark = new BMIPerson({weight: 78, height: 169})
+const John = new BMIPerson({weight: 92, height: 195})
+
+const higherBMI = Mark.calculateBMI() > John.calculateBMI() ? "Mark" : "John";
+
+
+console.log("Higheer BMI has ", higherBMI);*/
+
+/*const John = {
+    weight: 92,
+    height: 200
+}
+
+const Mark = {
+    weight: 80,
+    height: 190
+}
+
+function calculateBMI(obj){
+     return this.weight / (this.height * this.height);
+}
+
+function calculateHighestBMI({john: obj1, mark: obj2}){
+    return calculateBMI(obj1) > calculateBMI(obj2) ? "John" : "Mark" 
+}*/
