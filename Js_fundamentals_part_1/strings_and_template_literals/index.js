1ed0da0e7b961411001916e294fbae81b6582e4a
Komron <kr135369@gmail.com>
2023-11-15 15:17:14 +0500

    string adn template literals

---
+++ Js_fundamentals_part_1/strings_and_template_literals/index.js
@@ -0,0 +1,16 @@
+const firstname = 'jonas';
+const job = 'teacher';
+const birthYear = '1991';
+const year = '2037';
+
+const jonas = "i'm" + firstname + ', a' + (year-birthYear) + 'year old' + job  + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstname} , a ${year - birthYear} year old ${job} !`;
+console.log(jonasNew)
+
+console.log(`just a regular string...`);
+
+console.log("string with \n\
+multiple \n\
+lines ");
