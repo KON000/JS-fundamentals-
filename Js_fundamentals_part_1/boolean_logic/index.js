c8ba09ff52aa80adeaa637194d8b68831e38e77e
Komron <kr135369@gmail.com>
2023-11-17 14:36:10 +0500

    true, false

---
+++ Js_fundamentals_part_1/boolean_logic/index.js
@@ -0,0 +1,13 @@
+const age = 16;
+if(age >= 20) {  // A
+    console.log('true');
+}
+    else(age < 30) {  // B
+        console.log('false')
+    }
+
+    // not A = true
+    // A and B = false
+    // A or B = true
+    // not A and B = true
+    // A or !B = false
