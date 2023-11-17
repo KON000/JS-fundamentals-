a49e05bde7324fce31bfc7e861b29048ffdf987e
Komron <kr135369@gmail.com>
2023-11-17 17:10:39 +0500

    code_chalenges3

---
+++ Js_fundamentals_part_1/code_chalenges3/index.js
@@ -0,0 +1,14 @@
+// const dolphins = [96, 108, 89];
+// const koalas = [88, 91, 110];
+
+// const calculateAverage = (numbers) => {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     sum += numbers[i];
+//   }
+//   return sum / numbers.length;
+// };
+
+// console.log(calculateAverage(dolphins));
+// console.log(calculateAverage(koalas));
+// console.log(calculateAverage(dolphins) > calculateAverage(koalas));
